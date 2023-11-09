ae782d8e6d461cdc675c4d2f0fb23e104328c7e6
KimYongSae <yonggale3@naver.com>
2023-11-09 17:54:12 +0900

    11/09

---
+++ sql/dummy.sql
@@ -0,0 +1,3 @@
+insert into users VALUES('yonggale3@naver.com', 'abcd', '김용세');
+
+insert into board (user_id, title, content) VALUES('yonggale3@naver.com', 'test', 'testtest');
--- src/main/java/com/example/myadminproject/controller/HelloController.java
+++ src/main/java/com/example/myadminproject/controller/HelloController.java
@@ -11,43 +11,51 @@ public class HelloController
     @GetMapping("/")
     public String hello(){
 
+        log.info("helloController");
         return "index";
     }
 
     @GetMapping("/grid")
     public String grid(){
 
+        log.info("gridController");
         return "grid";
     }
 
     @GetMapping("/board")
     public String board(){
 
+        log.info("boardController");
         return "board";
     }
     @GetMapping("/excel")
     public String excel(){
 
+        log.info("excelController");
         return "excel";
     }
     @GetMapping("/chart")
     public String chart(){
 
+        log.info("chartController");
         return "chart";
     }
     @GetMapping("/lostark")
     public String lostark(){
 
+        log.info("lostarkController");
         return "lostark";
     }
     @GetMapping("/login")
     public String login(){
 
+        log.info("loginController");
         return "login";
     }
     @GetMapping("/contact")
     public String contact(){
 
+        log.info("contactController");
         return "contact";
     }
 }
